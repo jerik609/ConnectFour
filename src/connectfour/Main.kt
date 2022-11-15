115f2846de30eb46a232b8b9b4dd2ffe559adb36
Michal Jancok <michal.jancok@gmail.com>
2022-11-16 00:27:03 +0100

    added board drawing and moved code to functions

--- src/connectfour/Main.kt
+++ src/connectfour/Main.kt
@@ -2,17 +2,12 @@ package connectfour
 
 import java.util.Scanner
 
-fun main() {
-    val scanner = Scanner(System.`in`)
+fun getPlayerName(scanner: Scanner, query: String): String {
+    println(query)
+    return scanner.nextLine()
+}
 
-    println("Connect Four")
-
-    println("First player's name:")
-    val playerOne = scanner.nextLine()
-
-    println("Second player's name:")
-    val playerTwo = scanner.nextLine()
-
+fun getBoardSize(scanner: Scanner): Pair<Int, Int> {
     var boardSize = Pair(0, 0)
     do {
         println("Set the board dimensions (Rows x Columns)")
@@ -43,7 +38,52 @@ fun main() {
         }
 
     } while (boardSize.first == 0)
+    return boardSize
+}
 
+fun printBoard(rows: Int, columns: Int) {
+    // print numbers on top
+    for (column in 1..columns) {
+        print(" $column")
+    }
+    println()
+
+    // print actual board
+    for (row in 1..rows)
+    {
+        for (column in 1..columns) {
+            print("║ ")
+            if (column == columns) print('║')
+        }
+        println()
+    }
+
+    // print bottom
+    for (column in 1..columns) {
+        if (column == 1) {
+            print("╚═")
+        }
+        else {
+            print("╩═")
+            if (column == columns) print("╝")
+        }
+    }
+}
+
+fun displayGameState(playerOne: String, playerTwo: String, boardSize: Pair<Int, Int>) {
     println("$playerOne VS $playerTwo")
     println("${boardSize.first} X ${boardSize.second} board")
+    printBoard(boardSize.first, boardSize.second)
+}
+
+fun main() {
+    val scanner = Scanner(System.`in`)
+
+    println("Connect Four")
+
+    val playerOne = getPlayerName(scanner, "First player's name:")
+    val playerTwo = getPlayerName(scanner, "Second player's name:")
+    val boardSize = getBoardSize(scanner)
+
+    displayGameState(playerOne, playerTwo, boardSize)
 }
