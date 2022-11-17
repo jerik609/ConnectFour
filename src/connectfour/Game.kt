1522e6b5433135d879f1249f395b155aa2ae3487
Michal Jancok <michal.jancok@gmail.com>
2022-11-17 23:08:42 +0100

    add win/draw evaluation

--- src/connectfour/Game.kt
+++ src/connectfour/Game.kt
@@ -20,16 +20,80 @@ fun switchPlayer(
     }
 }
 
-fun makeMove(currentPlayer: Pair<String, Char>, move: Int, gameBoard: MutableList<MutableList<Char>>) {
+fun makeMove(currentPlayer: Pair<String, Char>, move: Int, gameBoard: MutableList<MutableList<Char>>): Pair<Int, Int> {
     with(gameBoard[move - 1].indexOfFirst { it == EMPTY_FIELD }) {
         // println("adding ${currentPlayer.second} to position column $move and row $this")
         gameBoard[move - 1][this] = currentPlayer.second
+        return Pair(move - 1, this)
     }
 }
 
-fun hasWon(currentPlayer: Pair<String, Char>, gameBoard: MutableList<MutableList<Char>>): Boolean {
-    return false
+fun check4(
+    currentPlayer: Pair<String, Char>,
+    column: Int,
+    row: Int,
+    stepColumn: Int,
+    stepRow: Int,
+    gameBoard: MutableList<MutableList<Char>>
+): Boolean {
+    var count = 0
+    var c = column
+    var r = row
+    for (i in 1..4) {
+        if (gameBoard[c][r] != currentPlayer.second) {
+            break // other player's disc, sequence broken, can stop checking in this direction
+        }
+        if (gameBoard[c][r] == currentPlayer.second) {
+            // print(gameBoard[c][r]) // debug
+            count++ // sequence continues, increase counter
+        }
+        c += stepColumn
+        r += stepRow
+        if (c !in 0..gameBoard.lastIndex || r !in 0..gameBoard[0].lastIndex) {
+            break // edge of board reached, can stop checking in this direction
+        }
+    }
+    // println() // debug
+    return count == 4
 }
+
+fun isBoardFull(gameBoard: MutableList<MutableList<Char>>): Boolean {
+    for (i in 0..gameBoard.lastIndex) {
+        if (gameBoard[i].last() == ' ') {
+            return false
+        }
+    }
+    return true
+}
+
+/**
+ * @param currentPlayer player whose move it is now
+ * @param lastMove coordinates of the last move made by current player
+ * @param gameBoard the status of the game
+ * @return true if current player has won
+ */
+fun getGameResult(
+    currentPlayer: Pair<String, Char>,
+    lastMove: Pair<Int, Int>,
+    gameBoard: MutableList<MutableList<Char>>
+): Int = if (
+        check4(currentPlayer, lastMove.first, lastMove.second, 1, 0, gameBoard) ||   // column +
+        check4(currentPlayer, lastMove.first, lastMove.second, -1, 0, gameBoard) ||  // column -
+        check4(currentPlayer, lastMove.first, lastMove.second, 0, 1, gameBoard) ||   // row +
+        check4(currentPlayer, lastMove.first, lastMove.second, 0, -1, gameBoard) ||  // row -
+        check4(currentPlayer, lastMove.first, lastMove.second, 1, 1, gameBoard) ||   // diagonal ++
+        check4(currentPlayer, lastMove.first, lastMove.second, -1, -1, gameBoard) || // diagonal --
+        check4(currentPlayer, lastMove.first, lastMove.second, 1, -1, gameBoard) ||  // diagonal +-
+        check4(currentPlayer, lastMove.first, lastMove.second, -1, 1, gameBoard)     // diagonal -+
+    ) {
+        1
+    } else {
+        if (isBoardFull(gameBoard)) {
+            0
+        } else {
+            -1
+        }
+    }
 
 fun playTheGame(
     playerOne: Pair<String, Char>,
@@ -40,24 +104,29 @@ fun playTheGame(
     displayGameState(playerOne, playerTwo, gameBoard)
 
     do {
+        // make move or end
         val move = getMove(currentPlayer, gameBoard)
-
         if (move == 0) {
-            println("Game over!")
             break
         }
 
-        makeMove(currentPlayer, move, gameBoard)
-
+        // perform the move and display new situation
+        val lastMove = makeMove(currentPlayer, move, gameBoard)
         printBoard(gameBoard)
 
-        if (hasWon(currentPlayer, gameBoard)) {
-            println("")
-            break
+        // evaluate game end
+        when (getGameResult(currentPlayer, lastMove, gameBoard)) {
+            1 -> {
+                println("Player ${currentPlayer.first} won")
+                break
+            }
+            0 -> {
+                println("It is a draw")
+                break
+            }
+            else -> currentPlayer = switchPlayer(currentPlayer, playerOne, playerTwo)
         }
 
-        currentPlayer = switchPlayer(currentPlayer, playerOne, playerTwo)
     } while (true)
-
-
+    println("Game Over!")
 }
